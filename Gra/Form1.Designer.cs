a365dcf678eb4600285519b3d786ff10df99967c
aniazajac <ania.zajac4@gmail.com>
2017-05-31 20:47:12 +0200

    Add project files.

---
+++ Gra/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Gra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.score1 = new System.Windows.Forms.Label();
+            this.score2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.LightBlue;
+            this.player1.Location = new System.Drawing.Point(291, 12);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(124, 21);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            this.player1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // player2
+            // 
+            this.player2.BackColor = System.Drawing.Color.Pink;
+            this.player2.Location = new System.Drawing.Point(291, 282);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(124, 21);
+            this.player2.TabIndex = 1;
+            this.player2.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Black;
+            this.ball.Location = new System.Drawing.Point(343, 145);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 19);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // score1
+            // 
+            this.score1.AutoSize = true;
+            this.score1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.score1.Location = new System.Drawing.Point(0, 0);
+            this.score1.Name = "score1";
+            this.score1.Size = new System.Drawing.Size(30, 34);
+            this.score1.TabIndex = 3;
+            this.score1.Text = "0";
+            // 
+            // score2
+            // 
+            this.score2.AutoSize = true;
+            this.score2.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.score2.Location = new System.Drawing.Point(0, 282);
+            this.score2.Name = "score2";
+            this.score2.Size = new System.Drawing.Size(30, 34);
+            this.score2.TabIndex = 4;
+            this.score2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(730, 315);
+            this.Controls.Add(this.score2);
+            this.Controls.Add(this.score1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label score1;
+        private System.Windows.Forms.Label score2;
+    }
+}
+
